68d77a0edd82ae3c0220bb62334c4846dd39b7a5
zwsut <zwsutherland@gmail.com>
2024-10-05 19:28:51 -0400

    Duplicate items in cart

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -21,22 +21,26 @@ export default class ShoppingCart {
     }
   }
 
-  // Template for cart items
+
   cartItemTemplate(item) {
+    const imageUrl = item.Image || (item.Images && item.Images.PrimaryLarge);
+  
     return `
       <li class="cart-card divider">
         <a href="#" class="cart-card__image">
-          <img src="${item.Image}" alt="${item.Name}" />
+          <img src="${imageUrl}" alt="${item.Name}" />
         </a>
         <a href="#">
           <h2 class="card__name">${item.Name}</h2>
         </a>
-        <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-        <p class="cart-card__quantity">qty: 1</p>
+        <p class="cart-card__color">${item.Colors ? item.Colors[0].ColorName : ''}</p>
+        <p class="cart-card__quantity">qty: ${item.quantity}</p>
         <p class="cart-card__price">$${item.FinalPrice}</p>
         <span data-id="${item.Id}" class="remove-item">X</span>
       </li>`;
   }
+  
+  
 
   // Method to update the total amount
   calculateCartTotal() {
