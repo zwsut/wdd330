8513eaf88d9965d199078341ee5ddad7eb6beadb
wstammSeidor <walterstamm92@gmail.com>
2024-10-05 17:41:59 -0300

    update

---
+++ src/js/product-listing.js
@@ -0,0 +1,14 @@
+import ProductListing from './ProductList.mjs';
+import ProductData from './ProductData.mjs';
+import {  loadHeaderFooter } from './utils.mjs';
+
+const dataSource = new ProductData('tents');
+
+const productList = new ProductListing('tents', dataSource, '.product-list');
+productList.init();
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadHeaderFooter();
+});
