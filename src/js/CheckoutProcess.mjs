bf530eddd5804abed9f802b6f5f9a14aea2af11f
wstammSeidor <walterstamm92@gmail.com>
2024-10-08 22:22:21 -0300

    update

---
+++ src/js/CheckoutProcess.mjs
@@ -0,0 +1,72 @@
+import { getLocalStorage } from "./utils.mjs";
+
+export default class CheckoutProcess {
+    constructor(key, outputSelector) {
+      this.key = key;
+      this.outputSelector = outputSelector;
+      this.list = [];
+      this.itemTotal = 0;
+      this.shipping = 0;
+      this.tax = 0;
+      this.orderTotal = 0;
+    }
+  
+    init() {
+      this.list = getLocalStorage(this.key);
+      this.calculateItemSummary();
+    }
+  
+    calculateItemSummary() {
+
+        this.list.forEach(item => {
+            this.itemTotal += item.FinalPrice * (item.quantity || 1);
+        });
+        this.itemTotal = parseFloat(this.itemTotal.toFixed(2));
+        this.calculateOrdertotal();
+        this.displayOrderTotals();
+    }
+  
+    calculateOrdertotal() {
+      // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
+      console.log(this.list.length);
+      if(this.list.length === 1)
+        {
+          this.shipping = 10;
+        }
+        else if(this.list.length > 1)
+        {
+          this.shipping = 10 + (this.list.length - 1) * 2;
+        }
+        else
+        {
+          this.shipping = 0;
+        }
+      // Calcular el impuesto
+      this.tax = this.itemTotal * 0.06;
+      
+      // Calcular el total del pedido
+      this.orderTotal = this.itemTotal + this.shipping + this.tax;
+      
+      // Redondear a dos decimales
+      this.shipping = parseFloat(this.shipping.toFixed(2));
+      this.tax = parseFloat(this.tax.toFixed(2));
+      this.orderTotal = parseFloat(this.orderTotal.toFixed(2));
+      this.displayOrderTotals();
+
+      // display the totals.
+      this.displayOrderTotals();
+    }
+  
+    displayOrderTotals() {
+      // once the totals are all calculated display them in the order summary page
+      const subtotalElement = document.querySelector('#subtotal');
+      const shippingElement = document.querySelector('#shipping');
+      const taxElement = document.querySelector('#tax');
+      const orderTotalElement = document.querySelector('#order-total');
+
+      subtotalElement.textContent = this.itemTotal;
+      shippingElement.textContent = this.shipping;
+      taxElement.textContent = this.tax;
+      orderTotalElement.textContent = this.orderTotal;
+    }
+  }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,5 +1,9 @@
 import { loadHeaderFooter } from './utils.mjs';
+import CheckoutProcess from './CheckoutProcess.mjs';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   loadHeaderFooter();
 });
+const myCheckout = new CheckoutProcess('so-cart', '.checkout-summary');
+myCheckout.init();
