7a95ce7effbc4b6e7526fe01aac9b515c4dcf411
nefiserrano <nefi_3902@hotmail.com>
2024-10-15 23:42:47 -0600

    Cart quantities completed

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -16,6 +16,7 @@ export default class ShoppingCart {
     if (Array.isArray(cartItems) && cartItems.length > 0) {
       renderListWithTemplate(this.cartItemTemplate, this.listElement, cartItems, 'afterbegin', true);
       this.addEventListenersToRemoveButtons(); // Adds listeners to remove buttons
+      this.calculateQuantity();
     } else {
       this.listElement.innerHTML = '<p>Your cart is empty.</p>';
     }
@@ -34,25 +35,53 @@ export default class ShoppingCart {
           <h2 class="card__name">${item.Name}</h2>
         </a>
         <p class="cart-card__color">${item.Colors ? item.Colors[0].ColorName : ''}</p>
-        <p class="cart-card__quantity">qty: ${item.quantity}</p>
+        <label for="quantity">Quantity:</label>
+        <select name="quantity" class="quantity">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
         <p class="cart-card__price">$${item.FinalPrice}</p>
         <span data-id="${item.Id}" class="remove-item">X</span>
       </li>`;
   }
   
-  
+  // This function gets the value from the quantity select button
+
+  calculateQuantity() {
+    const quantity = document.querySelectorAll('.quantity');
+    const quantities = [];
+    quantity.forEach((selectButton) => {
+      selectButton.addEventListener('change', this.updateTotalCart);
+      var value = selectButton.value;
+      value = parseInt(value);
+      quantities.push(value);
+    });
+    return quantities;
+  }
 
   // Method to update the total amount
   calculateCartTotal() {
     const cartItems = getLocalStorage('so-cart') || [];
     let total = 0;
-    cartItems.forEach((item) => {
-      total += item.FinalPrice;
-    });
+    var quantities = []
+    quantities = this.calculateQuantity();
+    let subtotal = 0
+    for (let i = 0; i < quantities.length; i++) {
+      subtotal = quantities[i] * cartItems[i].FinalPrice;
+      total += subtotal
+    }  
     return total.toFixed(2);
   }
 
-  updateTotalCart() {
+  updateTotalCart = () => {
     const cartFooter = document.querySelector('.cart-footer');
     const cartItems = getLocalStorage('so-cart') || [];
 
